675d438c0f3a337c5c96eaacdc45f34c538e4744
ivonne-arango <ivonne.arango@ibm.com>
2020-08-15 20:05:58 -0500

    test2

--- test_1.py
+++ test_1.py
@@ -1,5 +1,5 @@
 def berlinClock(time):
-    HH = int(time[0:1])
+    HH = int(time[0:2])
     MM = int(time[3:5])
     SS = int(time[6:9])
     HR5 = int(HH/5)
@@ -11,11 +11,21 @@ def berlinClock(time):
     SalidaF =""
     if (SD1 == 0):
         SD = "Y"
+    print ("HH: " ,HH)    
+    print ("MM: ", MM)    
+    print ("SS: ", SS)    
+    print ("HR5: ", HR5)    
+    print ("HR1: ", HR1)    
+    print ("MN5: ", MN5)    
+    print ("MN1: ", MN1)    
+    print ("SD1: ",SD1)    
+    print ("SD: ",SD)    
 
 #Ciclo horas 5
     i=0
     salidaH1 = ["O","O","O","O"]
     while i < HR5:
+        print (i) 
         salidaH1[i]="R"      
         i += 1
     SalidaF = SalidaF + "\n" + str(salidaH1)  
@@ -43,19 +53,24 @@ def berlinClock(time):
         salidaM2[i]="Y"
         i += 1
     SalidaF = SalidaF + "\n" + str(salidaM2)
-    print  (SD + SalidaF)
-    return SD + SalidaF 
+    SalidaF = SalidaF.replace(",","")
+    SalidaF = SalidaF.replace("'","")
+    SalidaF = SalidaF.replace("[","")
+    SalidaF = SalidaF.replace("]","")
+    SalidaF = SalidaF.replace(" " ,"")
+    print  (str(SD + SalidaF))
+    return str(SD + SalidaF) 
 
 def test_one():
         assert  berlinClock("00:00:00") == ("Y\nOOOO\nOOOO\nOOOOOOOOOOO\nOOOO")
 def test_two():
-        assert  berlinClock("12:56:02") == ("Y\nRROO\nRROO\nYYRYYRYYRYY\nYOOO")
+        assert  berlinClock("12:56:01") == ("Y\nRROO\nRROO\nYYRYYRYYRYY\nYOOO")
 def test_three():
         assert  berlinClock("22:32:45") == ("O\nRRRR\nRROO\nYYRYYROOOOO\nYYOO")
 def test_four():
         assert  berlinClock("01:01:01") == ("O\nOOOO\nROOO\nOOOOOOOOOOO\nYOOO")
 def test_five():
         assert  berlinClock("23:59:59") == ("O\nRRRR\nRRRO\nYYRYYRYYRYY\nYYYY")
-def test_five():
+def test_six():
         assert  berlinClock("00:00:01") == ("O\nOOOO\nOOOO\nOOOOOOOOOOO\nOOOO")
 
