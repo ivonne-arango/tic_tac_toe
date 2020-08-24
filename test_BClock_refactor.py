41b333504235c7fd1872713860ab0b3b81021b2d
ivonne-arango <ivonne.arango@ibm.com>
2020-08-23 22:13:52 -0500

    Refactor_5_hours

---
+++ test_BClock_refactor.py
@@ -0,0 +1,22 @@
+ # MAIN Function
+def berlinClock(time):
+    HH = int(time[0:2])
+    HR5 = int(HH/5)
+#Ciclo horas 5
+    i=0
+    salidaH1 = ["O","O","O","O"]
+    while i < HR5:
+        salidaH1[i]="R"      
+        i += 1
+    SalidaF = str(salidaH1) 
+    SalidaF = SalidaF.replace(",","")
+    SalidaF = SalidaF.replace("'","")
+    SalidaF = SalidaF.replace("[","")
+    SalidaF = SalidaF.replace("]","")
+    SalidaF = SalidaF.replace(" " ,"")
+    return str(SalidaF)
+
+
+#Test for 5 hours indicator
+def test_5_hours():
+        assert  berlinClock("12:00:00") == ("RROO")
