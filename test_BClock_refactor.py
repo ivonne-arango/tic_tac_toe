867141640971624b054e9908416605dd8da78232
ivonne-arango <ivonne.arango@ibm.com>
2020-08-26 19:00:26 -0500

    Clean_code_BClock

--- test_BClock_refactor.py
+++ test_BClock_refactor.py
@@ -1,80 +1,85 @@
 def validateTime(time):
-    HH =time[0:2]
-    MM =time[3:5]
-    SS =time[6:9]
-    lista1 = []
+    HoraValidate     = time[0:2]
+    MinutosValidate  = time[3:5]
+    SegundosValidate = time[6:9]
+    SalidaValidate   = []
 #Validate format
-    if HH.isdigit() == False or MM.isdigit() == False or SS.isdigit() == False: 
-        lista1.append(False)
-        lista1.append("Formato invalido")
-        return lista1
+    if HoraValidate.isdigit() == False or MinutosValidate.isdigit() == False or SegundosValidate.isdigit() == False: 
+        SalidaValidate.append(False)
+        SalidaValidate.append("Formato invalido")
+        return SalidaValidate 
 #Validate range for hours, minutes and seconds
-    if (int(HH) >= 0 and int(HH)<=23) and (int(MM) >= 0 and int(MM)<=59) and (int(SS) >= 0 and int(SS)<=59):
-        lista1.append(True)
+    if (int(HoraValidate) >= 0 and int(HoraValidate)<=23) and (int(MinutosValidate) >= 0 and int(MinutosValidate)<=59) and (int(SegundosValidate) >= 0 and int(SegundosValidate)<=59):
+        SalidaValidate.append(True)
     else:
-        lista1.append(False)
-        lista1.append("Hora fuera de rango")
-    
-    return lista1
+        SalidaValidate.append(False)
+        SalidaValidate.append("Hora fuera de rango")   
+    return SalidaValidate    
+
  # MAIN Function
 def berlinClock(time):
     validateTime(time)
-    lista2 = []
-    lista2 = validateTime(time)
-    if lista2[0] == False:
-        return lista2
-    HH  = int(time[0:2])
-    MM  = int(time[3:5])
-    SS  = int(time[6:9])
-    HR5 = int(HH/5)
-    HR1 = int(HH%5)
-    MN5 = int(MM/5)
-    MN1 = int(MM%5)
-    SD1 = int(SS%2)
-    SalidaF =""
-#Ciclo horas 5
+    SalidaBerlinErr   = []
+    SalidaBerlinErr   = validateTime(time)
+    if SalidaBerlinErr[0] == False:
+        return SalidaBerlinErr
+#Variables Definition
+    HorasBerlin     = int(time[0:2])
+    MinutosBerlin   = int(time[3:5])
+    SegungosBerlin  = int(time[6:9])
+    HorasBerlin5    = int(HorasBerlin/5)
+    HorasBerlin1    = int(HorasBerlin%5)
+    MinutosBerlin5  = int(MinutosBerlin/5)
+    MinutosBerlin1  = int(MinutosBerlin%5)
+    SegundosBerlin  = int(SegungosBerlin%2)
+    SalidaBerlin =""
+#Loop per 5 hours,variable i, is a counter
     i=0
-    salidaH1 = ["O","O","O","O"]
-    while i < HR5:
-        salidaH1[i]="R"      
+    SalidaHoras5 = ["O","O","O","O"]
+    while i < HorasBerlin5:
+        SalidaHoras5[i]="R"      
         i += 1
-    SalidaF = "\n" + str(salidaH1)
-#Ciclo por horas 1
-    salidaH2 = ["O","O","O","O"]
+    SalidaBerlin = "\n" + str(SalidaHoras5)
+#Loop per 1 hour, variable i, is a counter
+    SalidaHoras1 = ["O","O","O","O"]
     i=0
-    while i < HR1:
-        salidaH2[i]="R"
+    while i < HorasBerlin1:
+        SalidaHoras1[i]="R"
         i += 1  
-    SalidaF = SalidaF + "\n" + str(salidaH2)
-#Ciclo por minutos 5
-    salidaM1 = ["O","O","O","O","O","O","O","O","O","O","O"]
+    SalidaBerlin = SalidaBerlin + "\n" + str(SalidaHoras1)
+#Loop per 5 minutes, variable i, is a counter
+    SalidaMinutos5 = ["O","O","O","O","O","O","O","O","O","O","O"]
     i=0
-    while i< MN5:
+    while i< MinutosBerlin5:
         if (i == 2 or i==5 or i==8):
-            salidaM1[i]="R"
+            SalidaMinutos5[i]="R"
         else: 
-            salidaM1[i]="Y"
+            SalidaMinutos5[i]="Y"
         i += 1  
-    SalidaF = SalidaF + "\n" + str(salidaM1) 
-    #Ciclo por minuto
-    salidaM2 = ["O","O","O","O"]
+    SalidaBerlin = SalidaBerlin + "\n" + str(SalidaMinutos5) 
+#Loop per 1 minute, variable i, is a counter
+    SalidaMinutos1 = ["O","O","O","O"]
     i = 0
-    while i < MN1:
-        salidaM2[i]="Y"
+    while i < MinutosBerlin1:
+        SalidaMinutos1[i]="Y"
         i += 1
-    SalidaF = SalidaF + "\n" + str(salidaM2)
-    #Ciclo per seconds
-    SD = "O"  
-    if (SD1 == 0):
-        SD = "Y" 
+    SalidaBerlin = SalidaBerlin + "\n" + str(SalidaMinutos1)
+#Loop per seconds
+    SalidaSegundos = "O"  
+    if (SegundosBerlin == 0):
+        SalidaSegundos = "Y" 
 
-    SalidaF = SalidaF.replace(",","")
-    SalidaF = SalidaF.replace("'","")
-    SalidaF = SalidaF.replace("[","")
-    SalidaF = SalidaF.replace("]","")
-    SalidaF = SalidaF.replace(" " ,"")
-    return str(SD + SalidaF) 
+    SalidaBerlin = SalidaBerlin.replace(",","")
+    SalidaBerlin = SalidaBerlin.replace("'","")
+    SalidaBerlin = SalidaBerlin.replace("[","")
+    SalidaBerlin = SalidaBerlin.replace("]","")
+    SalidaBerlin = SalidaBerlin.replace(" " ,"")
+    return str(SalidaSegundos + SalidaBerlin) 
 
+
+#Test for correct hour esc1.
+def test_correct_hour_esc1():
+        assert  berlinClock("08:19:08") == ("Y\nROOO\nRRRO\nYYROOOOOOOO\nYYYY")
 #Test for invalid format hour.
 def test_invalid_format_hour():
         assert  berlinClock("AA:19:08") == [False,'Formato invalido']
@@ -93,12 +98,3 @@ def test_invalid_format_seconds():
 #Test for invalid range seconds.
 def test_invalid_range_seconds():
         assert  berlinClock("08:19:90") == [False,'Hora fuera de rango']
-#Test for correct hour esc1.
-def test_correct_hour_esc1():
-        assert  berlinClock("08:19:08") == ("Y\nROOO\nRRRO\nYYROOOOOOOO\nYYYY")
-#Test for correct hour esc2.
-def test_correct_hour_esc2():
-        assert  berlinClock("23:31:01") == ("O\nRRRR\nRRRO\nYYRYYROOOOO\nYOOO")
-#Test for correct hour esc3
-def test_correct_hour_esc3():
-        assert  berlinClock("00:47:59") == ("O\nOOOO\nOOOO\nYYRYYRYYROO\nYYOO")
