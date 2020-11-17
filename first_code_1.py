ac0f21b75b05708f82daf2a4af6feec9eb91c207
ivonne-arango <ivonne.arango@ibm.com>
2020-11-16 22:51:49 -0600

    prueba_ganador_x_vertical

--- first_code_1.py
+++ first_code_1.py
@@ -5,22 +5,25 @@
  # MAIN Function
 def Tic_tac_toe(data_array):
     i=0
-    ganador = ''
+    ganador = ""
     while i< 9:
-        if data_array[1] == 'X' and  data_array[2] == 'X' and data_array[3] == 'X':
-            ganador = 'X'
-        if data_array[1] == 'O' and data_array[2] == 'O' and data_array[3] == 'O':
+        if data_array[0] == "X" and  data_array[1] == "X" and data_array[2] == "X":
+            ganador = "X"
+            print ('x')
+        if data_array[0] == "O" and data_array[1] == "O" and data_array[2] == "O":
             ganador = 'O'
         i+=1
+        print ('while')
     if ganador == '':
-        salida = 'Empate'
+        print ('emp')
+        salida = "empate"
     else:
-        salida = 'Ganador: ' + ganador
+        salida = "Ganador: " + ganador
     return salida
 
 #Tests for invalid hour
 def test_gana_X_horizontal():
-        assert  Tic_tac_toe("X,X,X,O,O, ,X,0,X") == ['Ganador: X']     
+        assert  Tic_tac_toe(["X","X","X","O","O","","X","0","X"]) == 'Ganador: X'   
 def test_gana_X_vertical():
         assert  Tic_tac_toe("O,X,O,O,X,X,O,X,O") == ['Ganador: X']  
 def test_gana_X_diagonal():
