072287507fdac47125c69f397afac40bd5f0f8a8
ivonne-arango <ivonne.arango@ibm.com>
2020-11-17 17:37:15 -0600

    prueba_ganador_x_horizontal

--- first_code_1.py
+++ first_code_1.py
@@ -9,13 +9,20 @@ def Tic_tac_toe(data_array):
     while i< 9:
         if data_array[0] == "X" and  data_array[1] == "X" and data_array[2] == "X":
             ganador = "X"
-            print ('x')
-        if data_array[0] == "O" and data_array[1] == "O" and data_array[2] == "O":
+        if data_array[3] == "X" and  data_array[4] == "X" and data_array[5] == "X":
+            ganador = "X"
+        if data_array[6] == "X" and  data_array[7] == "X" and data_array[8] == "X":
+            ganador = "X"
+        if data_array[0] == "X" and  data_array[3] == "X" and data_array[6] == "X":
+            ganador = "X"
+        if data_array[1] == "O" and data_array[4] == "O" and data_array[2] == "O":
             ganador = 'O'
+        if data_array[6] == "O" and  data_array[7] == "O" and data_array[8] == "O":
+            ganador = "O"
+        if data_array[0] == "O" and  data_array[3] == "O" and data_array[6] == "O":
+            ganador = "O"
         i+=1
-        print ('while')
     if ganador == '':
-        print ('emp')
         salida = "empate"
     else:
         salida = "Ganador: " + ganador
@@ -25,7 +32,11 @@ def Tic_tac_toe(data_array):
 def test_gana_X_horizontal():
         assert  Tic_tac_toe(["X","X","X","O","O","","X","0","X"]) == 'Ganador: X'   
 def test_gana_X_vertical():
-        assert  Tic_tac_toe("O,X,O,O,X,X,O,X,O") == ['Ganador: X']  
+        assert  Tic_tac_toe(["O","X","O","O","X","X","O","X","O"]) == 'Ganador: X'  
 def test_gana_X_diagonal():
-        assert  Tic_tac_toe("X,O,X,X,X, ,O,O,X") == ['Ganador: X']                                    
+        assert  Tic_tac_toe(["X","O","X","X","X","","O","0","X"]) == 'Ganador: X'
+def test_gana_X_horizontal_con_vacios():
+        assert  Tic_tac_toe(["","","","X","X","X","","",""]) == 'Ganador: X'  
+        
+                                          
 
