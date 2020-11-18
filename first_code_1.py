ae55802b71eb8d179c568db249c2a8794237c0ff
ivonne-arango <ivonne.arango@ibm.com>
2020-11-18 09:35:11 -0600

    prueba_ganador_O_diagonal_EXITOSA

--- first_code_1.py
+++ first_code_1.py
@@ -7,6 +7,8 @@ def Tic_tac_toe(data_array):
     i=0
     ganador = ""
     while i< 9:
+        if data_array[2] == "X" and  data_array[5] == "X" and data_array[8] == "X":
+            ganador = "X"
         if data_array[0] == "X" and  data_array[1] == "X" and data_array[2] == "X":
             ganador = "X"
         if data_array[3] == "X" and  data_array[4] == "X" and data_array[5] == "X":
@@ -45,13 +47,17 @@ def test_gana_X_horizontal():
         assert  Tic_tac_toe(["X","X","X","O","O","","X","0","X"]) == 'Ganador: X'   
 def test_gana_X_vertical():
         assert  Tic_tac_toe(["O","X","O","O","X","X","","X","O"]) == 'Ganador: X'  
+def test_gana_O_vertical_con_vacios():
+        assert  Tic_tac_toe(["O","","X","","","X","","X","X"]) == 'Ganador: X'  
 def test_gana_X_diagonal():
         assert  Tic_tac_toe(["X","O","X","X","X","","O","0","X"]) == 'Ganador: X'
 def test_gana_X_horizontal_con_vacios():
         assert  Tic_tac_toe(["","","","X","X","X","","",""]) == 'Ganador: X' 
-def test_gana_O_horizontal_con_vacios():
+def test_gana_O_horizontal():
         assert  Tic_tac_toe(["O","O","O","X"," ","X","","",""]) == 'Ganador: O' 
-def test_gana_O_vertical_con_vacios():
+def test_gana_O_vertical():
         assert  Tic_tac_toe(["X","","O","","","O","X","X","O"]) == 'Ganador: O' 
-                                          
+def test_gana_O_diagonal():
+        assert  Tic_tac_toe(["O","","","X","O","","X","X","O"]) == 'Ganador: O' 
+                                      
 
